55a88fbf74782fafe4f1d1632b71f6e2edae18ee
Mark Andro Guevarra <mark.guevarra@itsrizzoli.it>
2025-11-29 15:34:05 +0100

    Added Activity and layout for Login, Register

--- app/src/main/java/com/example/animedex/LoginActivity.java
+++ app/src/main/java/com/example/animedex/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.example.animedex;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +20,31 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        EditText usernameInput = findViewById(R.id.edTextLogin);
+        EditText passwordInput = findViewById(R.id.edTextPassword);
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                String username = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+
+                loginUser(username, password);
+            }
+        });
+
+        TextView txtRegisterLink = findViewById(R.id.click_register_link);
+        txtRegisterLink.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_login), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
